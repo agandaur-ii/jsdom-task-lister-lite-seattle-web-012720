19faa2815945994553b0d1dead1ef02321485875
Agandaur <cjwagstaffe@gmail.com>
2020-03-10 11:22:30 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
 });
+
+function addItem(item) {
+  let newItem = document.createElement("li");
+  newItem.textContent = item;
+  console.dir(newItem);
+  document.querySelector("#tasks").appendChild(newItem);
+}
+
+const form = document.getElementById("create-task-form");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  console.dir(event.target)
+  const taskToAdd = event.target["new-task-description"].value;
+  addItem(taskToAdd);
+});
+
